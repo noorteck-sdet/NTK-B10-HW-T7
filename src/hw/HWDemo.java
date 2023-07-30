7322dcab0f1ca5c90c11256b577dcc7148cf0c34
ajmal <ajmal@localhost>
2023-07-30 14:19:16 -0400

    Hey team,  I push my code for the second time , please go ahead and get the updated code from GitHub. Thanks

--- src/hw/HWDemo.java
+++ src/hw/HWDemo.java
@@ -12,12 +12,14 @@ public class HWDemo {
 		
 		System.out.println("Reema 1");
 		
-<<<<<<< HEAD
+
 		System.out.println("Reema 2");
 		
 		System.out.println("Reema 3");
-=======
+
 		System.out.println("Joshua 2");
->>>>>>> branch 'master' of https://github.com/noorteck-sdet/NTK-B10-HW-T7.git
+		
+		System.out.println("Ajmal 2");
+
 	}
 }
